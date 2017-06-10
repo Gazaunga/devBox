9b40dc7f041ed664818a9b388a8f03e56b6852ae
Gazaunga <Gazaunga@gmail.com>
2017-06-10 15:50:12 -0400

    Create mv.sh

---
+++ mv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mv -f .bash_aliases ~/.bash_aliases
+mv -f .bash_functions ~/.bash_functions
+mv -f .bash_profile ~/.bash_profile
+mv -f .bashrc ~/.bashrc
+mv -f .env ~/.env
+mv -f .lesskey ~/.lesskey
+mv -f .nanorc ~/.nanorc
+mv -f .profile ~/.profile
+mv -f .screenrc ~/.screenrc
+mv -f gitctl ~/gitctl
+mv -f pok3r ~/pok3r
+mv -f .config ~/.config
+mv -f .local ~/.local
+mv -f bin ~/bin
