811988b936b94680cca1afa91f1dd8d2c6de9447
Gazaunga <Gazaunga@gmail.com>
2017-07-19 07:21:10 -0400

    Update devstrap.sh

--- bin/.scripts/devstrap.sh
+++ bin/.scripts/devstrap.sh
@@ -28,7 +28,7 @@ rbenv install 2.4.1
 rbenv global 2.4.1
 
 gem install bundler
-gem install jekyll mysql2 rails jekyll-pagedown
+gem install jekyll mysql2 rails
 
 ## Database
 sudo apt-get install mongodb
