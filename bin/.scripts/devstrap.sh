3a23c3bb990ec5e9d6f0705eb49f5da744668f7a
Gazaunga <Gazaunga@gmail.com>
2017-07-20 08:37:23 -0400

    Update devstrap.sh

--- bin/.scripts/devstrap.sh
+++ bin/.scripts/devstrap.sh
@@ -1,20 +1,20 @@
 ###### development tools
-sudo apt-get install build-essential python-dev git nodejs-legacy npm gnome-tweak-tool openjdk-8-jdk
+sudo apt-get -y install build-essential python-dev git nodejs-legacy npm gnome-tweak-tool openjdk-8-jdk
 
 #### Compiler Design  ##########
-apt-get install flex bison
+apt-get -y install flex bison
 
 ##### Networking tools  ########
-apt-get install libpcap-dev libnet1-dev rpcbind openssh-server
+apt-get -y install libpcap-dev libnet1-dev rpcbind openssh-server
 
-sudo apt-get install nmap
+sudo apt-get -y install nmap
 
 ##### Web Development
-sudo apt-get install apache2 apache2-utils libapache2-mod-php
-sudo apt-get install php php-mcrypt php-mysql php-mbstring php-dom
+sudo apt-get -y install apache2 apache2-utils libapache2-mod-php
+sudo apt-get -y install php php-mcrypt php-mysql php-mbstring php-dom
 
 #### Ruby Install
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
@@ -31,10 +31,10 @@ gem install bundler
 gem install jekyll mysql2 rails
 
 ## Database
-sudo apt-get install mongodb
-sudo apt-get install libsqlite3-dev
-sudo apt-get install mysql-server libmysqlclient-dev
+sudo apt-get -y install mongodb
+sudo apt-get -y install libsqlite3-dev
+sudo apt-get -y install mysql-server libmysqlclient-dev
 
 #### C/C++ advance tools
-sudoa apt-get install autoconf automake
+sudoa apt-get -y install autoconf automake
 
